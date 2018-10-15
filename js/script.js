50642bcfbb5a9e771bf632f2b58ab0875cd48faa
Arsalan Fazil (Wacky Dev) <wackydev@gmail.com>
2018-10-15 23:11:30 +0500

    added javascript biodata function

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// variables
+
+function bioData(firstName, lastName, job) {
+  var firstName = firstName;
+  var lastName = lastName;
+  var job = job;
+  console.log("First Name: " + firstName);
+  console.log("Last Name: " + lastName);
+  console.log("Job: " + job);
+}
+bioData("wacky", "dev", "developer");
+
